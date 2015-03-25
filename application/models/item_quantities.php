2c7caf2e2b211d61d38a0a307042e0ee3e4cbdc7
jekkos <jeroen.peelaerts@gmail.com>
2015-03-25 16:48:59 +0100

    Item quantities field cleaned up

--- application/models/item_quantities.php
+++ application/models/item_quantities.php
@@ -68,10 +68,7 @@ class Item_quantities extends CI_Model
 	{
 		$quantity_old = $this->get_item_quantity($item_id, $location_id, $unit_id);
 		$quantity_new = $quantity_old->quantity + intval($quantity_change);
-		$location_detail = array('item_id'=>$item_id,
-									'location_id'=>$location_id,
-									'unit_id'=>$unit_id,				
-									'quantity'=>$quantity_new);
+		$location_detail = array('quantity'=>$quantity_new);
 		return $this->save($location_detail,$item_id,$location_id,$unit_id);
 	}
 }
--- application/views/partial/item_units.php
+++ application/views/partial/item_units.php
@@ -1,19 +1,26 @@
-<?php $i = 0; ?>
-<?php foreach($item_units as $unit_id => $unit_data ) { ?>
-<?php $unit_name = $unit_data['unit_name']; ?>
-<div class="field_row clearfix" style="<?php echo $unit_data['deleted'] ? 'display:none;' : 'display:block;' ?>">    
-<?php echo form_label($this->lang->line('config_item_unit').' ' .++$i. ':', 'item_unit_'.$i ,array('class'=>'required wide')); ?>
-    <div class='form_field'>
-    <?php $form_data = array(
-        'name'=>'item_unit_'.$unit_id,
-        'id'=>'item_unit_'.$unit_id,
-    	'class'=>'item_unit valid_chars required',
-        'value'=>$unit_name); 
-    	$unit_data['deleted'] && $form_data['disabled'] = 'disabled';
-    	echo form_input($form_data);
-    ?>
-    </div>
-    <img class="add_item_unit" src="<?php echo base_url('images/plus.png'); ?>" />
-    <img class="remove_item_unit" src="<?php echo base_url('images/minus.png'); ?>" />
-</div>
-<?php } ?>
+<?php 
+$i = 0;
+foreach($item_units as $unit_id => $unit_data ) 
+{
+	if ($unit_id > 0) 
+	{
+		$unit_name = $unit_data['unit_name']; ?>
+		<div class="field_row clearfix" style="<?php echo $unit_data['deleted'] ? 'display:none;' : 'display:block;' ?>">    
+		<?php echo form_label($this->lang->line('config_item_unit').' ' .++$i. ':', 'item_unit_'.$i ,array('class'=>'required wide')); ?>
+		    <div class='form_field'>
+		    <?php $form_data = array(
+		        'name'=>'item_unit_'.$unit_id,
+		        'id'=>'item_unit_'.$unit_id,
+		    	'class'=>'item_unit valid_chars required',
+		        'value'=>$unit_name); 
+		    	$unit_data['deleted'] && $form_data['disabled'] = 'disabled';
+		    	echo form_input($form_data);
+		    ?>
+		    </div>
+		    <img class="add_item_unit" src="<?php echo base_url('images/plus.png'); ?>" />
+		    <img class="remove_item_unit" src="<?php echo base_url('images/minus.png'); ?>" />
+		</div>
+	}
+	<?php 
+} 
+?>
